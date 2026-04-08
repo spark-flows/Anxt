ef300d969a2f0f8b8a2415170b062262b8f79bd5
pdungarani <parthivdungarani007@gmail.com>
2026-04-08 21:22:09 +0530

    readme update

--- README.md
+++ README.md
@@ -18,3 +18,4 @@ samples, guidance on mobile development, and a full API reference.
 
 
 /// Key Store pass and every thing = ANxt123
+/// alias = key0
--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.tech.a_nxt"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -24,8 +24,8 @@ android {
         applicationId = "com.tech.a_nxt"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
